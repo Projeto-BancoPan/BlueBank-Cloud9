847401f54935f45c1b0fff5fb3ea7edc19ce7479
Luiz Nascimento <luns.wm@hotmail.com>
2021-12-12 21:15:26 -0300

    Atualizando EnderecoRepistory

---
+++ src/main/java/com/bancopan/cloud9/bluebank/repositories/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.bancopan.cloud9.bluebank.repositories;
+
+import com.bancopan.cloud9.bluebank.models.EnderecoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository  extends JpaRepository<EnderecoModel, Long> {
+}
